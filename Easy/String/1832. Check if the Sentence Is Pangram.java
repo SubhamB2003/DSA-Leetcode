553300c9ea35d3a072f84ed60ae5b49d5a5e43c0
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-01-28 10:02:35 +0530

    Daily Practice

---
+++ Easy/String/1832. Check if the Sentence Is Pangram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+
+        if (sentence.length() < 26)
+            return false;
+        // HashSet<Character> set = new HashSet<>();
+        // for(int i = 0; i < sentence.length(); i++) {
+        // set.add(sentence.charAt(i));
+        // }
+        // if(set.size() != 26) return false;
+        // return true;
+        char ch = 97;
+        for (int i = 0; i < 26; i++) {
+            if (sentence.indexOf(ch) == -1)
+                return false;
+            ch++;
+        }
+
+        return true;
+    }
+}
