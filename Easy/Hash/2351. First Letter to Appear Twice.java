82b8c0ba50337e933b0aea025e920835da9d6518
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-02-10 21:23:04 +0530

    Daily Practice

---
+++ Easy/Hash/2351. First Letter to Appear Twice.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public char repeatedCharacter(String s) {
+
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                return s.charAt(i);
+            } else {
+                set.add(s.charAt(i));
+            }
+        }
+
+        return ' ';
+    }
+}
