c51740acf277581a161f591391fca4a21804f5f7
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-02-19 00:48:48 +0530

    Daily Practice

---
+++ Easy/Hash/2154. Keep Multiplying Found Values by Two.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        
+        int find = original;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        
+        boolean isLoop = true;
+        while(isLoop) {
+            if(!set.contains(find)) break;
+            else find *= 2;
+        }
+
+        return find;        
+    }
+}
