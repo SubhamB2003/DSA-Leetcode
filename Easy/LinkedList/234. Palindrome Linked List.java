956682802388c594a6c5c3565f7dd051279c3ef9
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-02-20 21:36:18 +0530

    Daily Practice

---
+++ Easy/LinkedList/234. Palindrome Linked List.java
@@ -0,0 +1,32 @@
+
+//  Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        StringBuilder sb = new StringBuilder();
+
+        while (head != null) {
+            sb.append(head.val);
+            head = head.next;
+        }
+        String s = sb.toString();
+        return s.equals(sb.reverse().toString());
+    }
+}
