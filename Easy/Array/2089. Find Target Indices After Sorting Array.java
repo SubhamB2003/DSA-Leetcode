f1339892de23203ae0417f3daed279bfc2214e5a
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-03-20 22:03:03 +0530

    Daily Practice

---
+++ Easy/Array/2089. Find Target Indices After Sorting Array.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                list.add(i);
+            }
+        }
+        return list;
+
+    }
+}
