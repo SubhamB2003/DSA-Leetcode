b068e1b78a31242dc5658a5bd51f9a58da61766f
Subham Bhattacharjee <subhambhttachariya03@gmail.com>
2023-04-20 23:59:20 +0530

    Daily Practice

---
+++ Easy/Array/338. Counting Bits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] countBits(int n) {
+
+        int[] ans = new int[n + 1];
+
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = count(i);
+        }
+
+        return ans;
+    }
+
+    static int count(int n) {
+
+        int count = 0;
+
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n = n >>> 1;
+        }
+
+        return count;
+    }
+}
